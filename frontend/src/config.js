c3d16c8a8dd3518fdd036347c19643aa9c6673ce
Chandu <chandududdela0103@gmail.com>
2025-11-30 13:03:38 +0530

    Upt : Render Deploy Url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:4000";
+export const API_BASE_URL = "https://mini-audit-trail-2l2w.onrender.com";
 //Later in deployment, we’ll change this to the live backend URL.
